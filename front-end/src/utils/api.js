33d122ce1f972eb0443c9625ec17656eadf0a777
Stephany Cisneros <stephany@thinkful.com>
2022-03-14 12:08:41 -0400

    fixing api.js file for deployment

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -6,8 +6,11 @@
  import formatReservationTime from "./format-reservation-date";
  
  const API_BASE_URL =
-   process.env.REACT_APP_API_BASE_URL || "https://cors-anywhere.herokuapp.com/https://restaurant-app-final-client.herokuapp.com";
+   process.env.REACT_APP_API_BASE_URL || "https://restaurant-app-final-client.herokuapp.com";
+
+app.use(cors());
  
+
  /**
   * Defines the default headers for these functions to work with `json-server`
   */
