2829eda8a0dbf8ebb97e5aadbccd7c84ae22567a
Stephany Cisneros <stephany@thinkful.com>
2023-10-07 20:11:30 -0700

    fixing deploy

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -14,6 +14,14 @@ knex.migrate
     knex.destroy();
   });
 
+  const cors=require("cors");
+  const corsOptions ={
+     origin:'*', 
+     credentials:true,            //access-control-allow-credentials:true
+     optionSuccessStatus:200,
+  }
+  
+  app.use(cors(corsOptions)) // Use this after the variable declaration
 
 
 function listener() {
