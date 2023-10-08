2a25b1265d54b36b011d69ef5550a33b1288ae58
Stephany Cisneros <stephany@thinkful.com>
2023-10-07 21:03:32 -0700

    fixing deploy

--- back-end/knexfile.js
+++ back-end/knexfile.js
@@ -8,10 +8,10 @@ require("dotenv").config();
 const path = require("path");
 
 const {
-  DATABASE_URL = "postgres://ibjtxlgp:NnsSj0DDUkZzXinJF9q1ndGzSZpePrsj@castor.db.elephantsql.com/ibjtxlgp",
+  DATABASE_URL = "postgres://cfwvayuq:a9sA8WS4d8d5km4KxQkn2Cjr1HJ5SCj0@peanut.db.elephantsql.com/cfwvayuq",
   DATABASE_URL_DEVELOPMENT = "postgres://ibjtxlgp:NnsSj0DDUkZzXinJF9q1ndGzSZpePrsj@castor.db.elephantsql.com/ibjtxlgp",
-  DATABASE_URL_TEST = "postgres://ibjtxlgp:NnsSj0DDUkZzXinJF9q1ndGzSZpePrsj@castor.db.elephantsql.com/ibjtxlgp",
-  DATABASE_URL_PREVIEW = "postgres://ibjtxlgp:NnsSj0DDUkZzXinJF9q1ndGzSZpePrsj@castor.db.elephantsql.com/ibjtxlgp",
+  DATABASE_URL_TEST = "postgres://vdpffkqh:n4HwqKZ-zx-7qZaMN85Q8qdEkXrT8Kat@castor.db.elephantsql.com/vdpffkqh",
+  DATABASE_URL_PREVIEW = "postgres://hojdzmrn:kQBkFV6uxQQpLdInfhmGaBcMHIt5I7SD@castor.db.elephantsql.com/hojdzmrn",
   DEBUG,
 } = process.env;
 
